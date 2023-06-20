de6d8a6bb57d36fe4cc0cbc669677d6f73dacece
jsoto <jsoto@freewheel.com>
2023-06-20 07:47:42 -0400

    draw logic, and fixes

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const board = document.querySelector("#gameBoard");
 const information = document.querySelector("#information");
+//const resetGame = document.getElementById("#resetGame");
 const boardStartingCells = ["", "", "", "", "", "", "", "", ""];
 
 let go = "circle";
+let playerMove = 0; // tracks player movement for draw game
 information.textContent = "Circle goes first";
 
 const addGo = (e) => {
@@ -12,8 +14,10 @@ const addGo = (e) => {
 
   if (go === "circle") {
     go = "cross";
+    playerMove++
   } else {
     go = "circle";
+    playerMove++
   }
 
   information.textContent = "Its now turn for " + go;
@@ -36,47 +40,77 @@ const checkScore = () => {
     [2, 4, 6], // cross matches
   ];
 
+  console.log('original Wining boxes: ',winningBoxes)
+
   winningBoxes.forEach((array) => {
     let circleWins = false;
     let crossWins = false;
     let hasCirclesArray = [];
     let hasCrossArray = [];
 
+    console.log('each array: ', array)
+
     array.forEach((cell) => {
-      console.log(cell);
+    console.log('forEach cell: ',cell);
 
       // Gets element ID based on cell to push to array
       const square = document.getElementById(`${cell}`);
+      
+      console.log('getting the ID for each cell (square): ', square)
 
-      // checks if we have class of 'circle' or 'cross' present
+      // checks if we have class of 'circle' or 'cross' is true
       const hasCircle = !!square.querySelector(".circle");
       const hasCross = !!square.querySelector(".cross");
+
+      console.log('checks if query selector .circle class is true: ', hasCircle)
+      console.log('checks if query selector .circle class is true: ', hasCross)
 
       // pushes index to has circle or corss array
       hasCirclesArray.push(hasCircle);
       hasCrossArray.push(hasCross);
+
+      console.log('pushes boolean value if true to an array called hasCircleArray', hasCirclesArray)
+      console.log('pushes boolean value if true to an array called hasCrossArray', hasCrossArray)
     });
 
     // checks all items in array and returns a boolan value
+    // every() method check for items in array and returns a boolean value 
     circleWins = hasCirclesArray.every((item) => item);
     crossWins = hasCrossArray.every((item) => item);
 
-    // if all items in array are true, then show message who wins
+    // since every method above returns a boolean value, we want to pick the winner and display win message
+    // & execute the function to remove further clicks on board game
     if (circleWins) {
       information.textContent = "Circle wins";
-      const removeCircleListener = document.querySelector('.circle')
-      removeCircleListener.removeEventListener("click", addGo)
+      removeEventListener()
+    } else if (playerMove === 9) {
+      information.textContent = "Game draw!";
     }
 
+    // same as above for crossWins
     if (crossWins) {
       information.textContent = "Cross wins";
+      removeEventListener()
+    } else if (playerMove === 9) {
+      information.textContent = "Game draw!";
     }
   });
 };
 
+
+const removeEventListener = () => {
+  boardStartingCells.forEach((cell, index) => {
+    // console.log('this are the cells: ',cell)
+    // console.log('this are the index: ',index)
+    const div = document.getElementById(`${index}`)
+    div.removeEventListener("click", addGo)
+  })
+}
+
+
 const createBoard = () => {
   boardStartingCells.forEach((cell, index) => {
-    console.log(cell, index);
+    //console.log(cell, index);
     const cellElement = document.createElement("div");
     cellElement.classList.add("square");
     cellElement.id = index;
@@ -86,3 +120,8 @@ const createBoard = () => {
 };
 
 createBoard();
+
+
+const restartGame = () => {
+  window.location.reload();
+}
